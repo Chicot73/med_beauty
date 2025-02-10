812212a8c1d3c24ca5063b476a6fda9498c8702c
igor <admin@igorbobyrev.ru>
2025-02-10 22:49:19 +0300

    last commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,57 @@
+//выбор языка
 
-// бургер-иеню
+let langwRandome = document.querySelector('.header__chose-lang'),
+  randomePot = document.querySelector('.header__randome'),
+  langwText = langwRandome.firstElementChild,
+  counter = 0;
+
+langwRandome.addEventListener('mouseover', inn);
+langwRandome.addEventListener('click', inn);
+
+function inn() {
+  randomePot.setAttribute("id", "list-start");
+}
+
+randomePot.addEventListener('mouseout', () => {
+  randomePot.removeAttribute("id", "list-start");
+})
+
+
+
+
+
+//функция не работает, если не повесить onclick на каждую кнопку в списке (не через addevlis, а через тег onclick)
+function listener(event) {
+  let el = event.target;
+
+  if (el) {
+
+    let a = el.textContent,
+      b = langwText.textContent;
+
+    let tmp = a;
+    a = b;
+    b = tmp;
+    langwText.textContent = b;
+    el.textContent = a;
+
+    function goodby() {
+
+      randomePot.removeAttribute("id", "list-start");
+
+    }
+    setTimeout(goodby, 200);
+  }
+}
+
+
+
+// бургер-меню
 
 let burgerMenu = document.querySelector('.header__burger'),
   burgerBtn = document.querySelector('.header__burger-btn'),
   burgerClose = document.querySelector('.header__cross'),
-  headerCover = document.querySelector('.header__cover'),
+  headerCover = document.querySelector('.header__cover'), //затемнение на фоне
   burgerLinks = document.getElementById('header_links'),
   upForm = document.querySelector(".header__form"),
   burgerGetinfo = document.querySelector('.header__form-btn'),
@@ -13,13 +60,33 @@ let burgerMenu = document.querySelector('.header__burger'),
 burgerBtn.addEventListener('click', (e) => {
 
   if (burgerMenu.classList.contains('header__burger--out')) {
-      burgerMenu.classList.remove('header__burger--out');
+    burgerMenu.classList.remove('header__burger--out');
   };
   burgerMenu.classList.add('header__burger--in');
   headerCover.style.display = 'block';
   body.style.overflow = 'hidden';
+
+  // document.addEventListener('click', nonFormClick);
+
   e.stopImmediatePropagation()
 });
+
+//Логика после открытия формы, если хотят закрыть его, нажав на пустое пространство вне формы
+
+
+// function nonFormClick(e) {
+
+//   let upformField = e.target.closest.upForm;
+
+//   if (!upformField) {
+//     burgerMenu.classList.remove('header__burger--in');
+//     upForm.style.display = 'none';
+//     headerCover.style.display = 'none';
+//     body.style.overflow = 'scroll';
+//   }
+// }
+
+//Логика после открытия бургер-меню, если хотят закрыть его
 
 let puArr = [burgerClose, burgerLinks].forEach(item => {
   item.addEventListener('click', (e) => {
@@ -27,142 +94,133 @@ let puArr = [burgerClose, burgerLinks].forEach(item => {
     headerCover.style.display = 'none';
     body.style.overflow = 'scroll';
     e.stopImmediatePropagation();
-    });
+  });
 });
 
 burgerGetinfo.addEventListener('click', (e) => {
   burgerMenu.classList.remove('header__burger--in');
   upForm.style.display = 'block';
-  e.stopImmediatePropagation();
+  // e.stopImmediatePropagation();
 });
+
+
+
 
 // верхняя форма
 
-let upperFormBtn = document.querySelector('.form__btn');
+let FormBtn = document.querySelectorAll('.form__btn');
 
-upForm.addEventListener('click', (e) => {
-  
-  if (e.target.closest('.form__btn')) {
 
-      sendForm();
 
+document.addEventListener('click', (e) => {
+
+  const target = e.target.closest('.form__btn');
+
+  if (target) {
+
+    burgerMenu.classList.remove('header__burger--in');
+    upForm.style.display = 'block';
+    // e.stopImmediatePropagation();
+
+    sendForm();
   }
 })
 
+
+
 //Маска формы
 
-let telSelector = document.querySelector('.tel-selector'),
+let telSelector = document.getElementById('phone'),
   nameSelector = document.getElementById('name'),
-  questionSelector = document.getElementById('question');
+  questionSelector = document.getElementById('question'),
+  telSelectorDown = document.getElementById('phone-f'),
+  nameSelectorDown = document.getElementById('name-f'),
+  questionSelectorDown = document.getElementById('question-f');
 
-// let telSelectorDown = document.querySelector('.tel-selector'),
-//     nameSelectorDown = document.getElementById('name-f'),
-//   questionSelectorDown = document.getElementById('question-f');  
+// Inputmask({ "mask": "+9[99](9[9999])[9[9999]]-[9[9999]]", "greedy": "false" }).mask(telSelector);
+// Inputmask({ "mask": "+9{1,20}/(/)/.-", "greedy": "false" }).mask(telSelectorDown);
 
-Inputmask({ "mask": "+7(999)999-99-99", "greedy": "true" }).mask(telSelector);
+const telValid = new Inputmask({
+  mask: "+*{1,20}",
+  greedy: false,
+  definitions: {
+    '*': {
+      validator: "[0-9\u0028\u0029\u002d]",//валидация пропускает только числа от 0 до 9, дефис и скобки
+    }
+  }
+});
+
+telValid.mask(telSelector); //применяем сформированную маску для указанного инпута emailSelector
 
 let sendForm = function () {
-    
-  document.addEventListener('submit', function(event) {
+
+  document.addEventListener('submit', function (event) {
     event.preventDefault();
-    let long = telSelector.value, 
+    let long = telSelector.value,
       long2 = nameSelector.value,
       long3 = questionSelector.value;
+    long4 = telSelectorDown.value;
+    long5 = nameSelectorDown.value;
+    long6 = questionSelectorDown.value;
     let longNum = long.replace(/[^0-9]/g, ""); //удаляем из строки всё, кроме цифр  // ошибка из-за этого
+    let longNum2 = long4.replace(/[^0-9]/g, "");
 
-    if (document.getElementById('agree').checked) {
-      
-      if (((longNum.length > 2) && (longNum.length < 50)) && ((long2.length > 1) && (long2.length < 36)) && ((long3.length > 2) && (long3.length < 1501))) {
-      
+    if ((document.getElementById('agree').checked) || (document.getElementById('agree-f').checked)) {
+
+      if ((((longNum.length > 2) && (longNum.length < 50)) && ((long2.length > 1) && (long2.length < 36)) && ((long3.length > 2) && (long3.length < 1501))) || (((longNum2.length > 2) && (longNum2.length < 50)) && ((long5.length > 1) && (long5.length < 36)) && ((long6.length > 2) && (long6.length < 1501)))) {
+
         document.getElementById('warning').style.display = "block";
+        document.getElementById('warning-f').style.display = "block";
         document.getElementById('warning').style.color = "#49ff0c";
+        document.getElementById('warning-f').style.color = "#49ff0c";
         document.getElementById('warning').textContent = "Спасибо, мы свяжемся с Вами!";
-    
+        document.getElementById('warning-f').textContent = "Спасибо, мы свяжемся с Вами!";
+
+        function delText() {
+          document.getElementById('warning').style.display = "none";
+          document.getElementById('warning-f').style.display = "none";
+        }
+        setTimeout(delText, 2000);
+
         function goodby() {
-            
+
           document.querySelector('.header__form').style.display = "none";
           headerCover.style.display = 'none';
           body.style.overflow = 'scroll';
 
         }
         setTimeout(goodby, 2000);
-          
+
       } else {
 
         document.getElementById('warning').textContent = "Пожалуйста введите правильные данные";
-        document.getElementById('warning').style.display = "block";
-
-      }
-    } else {
-
-      document.getElementById('warning').textContent = "Пожалуйста дайте свое согласие на обработку персональных данных";
-      document.getElementById('warning').style.display = "block";
-
-    }
-  })
-}
-
-// Hижняя форма
-
-/* downForm = document.querySelector(".more__form");
-
-downForm.addEventListener('click', (e) => {
-  
-  if (e.target.closest('.form__btn-down')) {
-
-      sendForm();
-
-  }
-}) */
-
-
-/* //Маска формы
-
-let sendFormDown = function () {
-  // let form = document.getElementById('form');
-    
-  document.addEventListener('submit', function(event2) {
-    event2.preventDefault();
-    let long = telSelectorDown.value,
-      long2 = nameSelectorDown.value,
-      long3 = questionSelectorDown.value;
-    let longNum = long.replace(/[^0-9]/g, ""); //удаляем из строки всё, кроме цифр
-
-    if (document.getElementById('agree-f').checked) {
-      
-      if (((longNum.length > 2) && (longNum.length < 50)) && ((long2.length > 1) && (long2.length < 36)) && ((long3.length > 2) && (long3.length < 1501))) {
-      
-        document.getElementById('warning').style.display = "block";
-        document.getElementById('warning').style.color = "#49ff0c";
-        document.getElementById('warning').textContent = "Спасибо, мы свяжемся с Вами!";
-    
-        function goodby() {
-            
-          document.querySelector('.more__form').style.display = "none";
-          headerCover.style.display = 'none';
-          body.style.overflow = 'scroll';
-
-        }
-        setTimeout(goodby, 2000);
-          
-      } else {
-
         document.getElementById('warning-f').textContent = "Пожалуйста введите правильные данные";
+        document.getElementById('warning').style.display = "block";
         document.getElementById('warning-f').style.display = "block";
 
       }
     } else {
 
+      document.getElementById('warning').textContent = "Пожалуйста дайте свое согласие на обработку персональных данных";
       document.getElementById('warning-f').textContent = "Пожалуйста дайте свое согласие на обработку персональных данных";
+      document.getElementById('warning').style.display = "block";
       document.getElementById('warning-f').style.display = "block";
-
     }
   })
-} */
+}
+
 
 // Hero-section Анимация появления
 
-function animStart () {
+alert = document.querySelector('.hero__alert');
+
+function alertStart() {
+
+  alert.setAttribute("id", "alert_start");
+
+}
+
+function loadPage() {
 
   let m1 = document.querySelector('.hero__module01'),
     m2 = document.querySelector('.hero__module02'),
@@ -170,8 +228,7 @@ function animStart () {
     m4 = document.querySelector('.hero__module04'),
     bigObg = document.querySelector('.hero__bigobj'),
     fog = document.querySelector('.hero__fog'),
-    blur = document.querySelector('.hero__blur'),
-    alert = document.querySelector('.hero__alert');
+    blur = document.querySelector('.hero__blur');
   m1.setAttribute("id", "module01");
   m2.setAttribute("id", "module02");
   m3.setAttribute("id", "module03");
@@ -179,60 +236,49 @@ function animStart () {
   bigObg.setAttribute("id", "bigdevice");
   fog.setAttribute("id", "fog");
   blur.setAttribute("id", "blur");
-  setTimeout(alertStart, 2000);
-
-  function alertStart() {
-
-    alert.setAttribute("id", "alert_start");
-
-    document.removeEventListener('click', animStart);
-
-  }
+  // setTimeout(alertStart, 2000);
 }
-
-document.addEventListener('click', animStart);
 
 // закрытие окна про куки
 
-function alertStart(ev) {
-
-  alert = document.querySelector('.hero__alert');
+function alertFinish(ev) {
 
   const area = ev.target.closest("#sub");
-    
-  if (area) { 
+  const area2 = ev.target.closest("#unsub");
+
+  if ((area) || (area2)) {
 
     alert.removeAttribute("id", "alert_start");
-
+    loadPage();
   }
 }
 
-body.addEventListener('click', alertStart);
+body.addEventListener('click', alertFinish);
 
 
 //accordeon
 
 let accordeon = document.querySelector('.questions__part2'),
-    tab = document.querySelectorAll('.questions__block'),
-    button = document.querySelectorAll('.questions__toform'),
-    arrows = document.querySelectorAll('.questions__toform'),
-    count;
+  tab = document.querySelectorAll('.questions__block'),
+  button = document.querySelectorAll('.questions__toform'),
+  arrows = document.querySelectorAll('.questions__toform'),
+  count;
 
 function click1(e) {
-    const target = e.target.closest('.questions__block');
+  const target = e.target.closest('.questions__block');
   if (target) {
-      
-    tab.forEach((item, i) => {
+
+    tab.forEach((item) => {
 
       let child = item.firstElementChild.lastElementChild;
-  
+
       if (item === target) {
 
-        
+
         if (item.classList.contains('mark')) {
 
           if (count == 1) {
-        
+
             child.removeAttribute("id", "block-act--in");
             child.setAttribute("id", "block-act--out");
             item.lastElementChild.style.transform = 'rotate(0deg)' + 'translate(0, -50%)';
@@ -240,7 +286,7 @@ function click1(e) {
             count++
 
           } else if (count == 2) {
-        
+
             child.removeAttribute("id", "block-act--out");
             child.setAttribute("id", "block-act--in");
             item.lastElementChild.style.transform = 'rotate(180deg)' + 'translate(0, 0)';
@@ -257,12 +303,12 @@ function click1(e) {
           item.lastElementChild.style.transform = 'rotate(180deg)' + 'translate(0, 0)';
           item.lastElementChild.style.top = '30px';
         }
-      } 
+      }
     })
   };
 };
 accordeon.addEventListener('click', (e) => {
-    click1(e);
+  click1(e);
 });
 
 
@@ -275,72 +321,72 @@ accordeon.addEventListener('click', (e) => {
 
 document.addEventListener('DOMContentLoaded', function () {
 
-    var tag = document.createElement('script');
-    tag.src = "https://www.youtube.com/iframe_api";
-    var firstScriptTag = document.getElementsByTagName('script')[0];
-    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+  var tag = document.createElement('script');
+  tag.src = "https://www.youtube.com/iframe_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-    var player;
+  var player;
 
-    let ytCross = document.getElementById('vcross1'),
-        backgro = document.getElementById('vframe1'),
-        ytBack = document.querySelector('.cover'),
-        mark = document.querySelector('.video__link'),
-        body = document.querySelector('body');
+  let ytCross = document.getElementById('vcross1'),
+    backgro = document.getElementById('vframe1'),
+    ytBack = document.querySelector('.cover'),
+    mark = document.querySelector('.video__link'),
+    body = document.querySelector('body');
 
-    mark.addEventListener('click', (e) => {
-        backgro.classList.add('active');
-        ytBack.classList.add('active');
-        body.style.overflow = 'hidden';
-        player = new YT.Player('player', {
-            height: '1080',
-            width: '100%',
-            videoId: mark.dataset.video,//из атрибута data-video кнопки
-            playerVars: { 'autoplay': 1, 'controls': 1 },
-            events: {
-                'onReady': onPlayerReady,
-                'onStateChange': onPlayerStateChange
-            }
-        });
-
-        function onPlayerReady(event) {
-            event.target.setVolume(30);
-            event.target.playVideo();
-            event.target.setPlaybackQuality('highres');
-        }
-
-        function onPlayerStateChange(event) {
-            if (event.data == YT.PlayerState.BUFFERING) {
-                event.target.setPlaybackQuality('highres');
-            }
-        }
-    })
-
-    let ytbgArr = [ytBack, ytCross];
-    ytbgArr.forEach(item => {
-        item.addEventListener('click', (e) => {
-            backgro.classList.remove('active');
-            ytBack.classList.remove('active');
-            body.style.overflow = 'scroll';
-            player.destroy();
-        });
+  mark.addEventListener('click', (e) => {
+    backgro.classList.add('active');
+    ytBack.classList.add('active');
+    body.style.overflow = 'hidden';
+    player = new YT.Player('player', {
+      height: '1080',
+      width: '100%',
+      videoId: mark.dataset.video,//из атрибута data-video кнопки
+      playerVars: { 'autoplay': 1, 'controls': 1 },
+      events: {
+        'onReady': onPlayerReady,
+        'onStateChange': onPlayerStateChange
+      }
     });
+
+    function onPlayerReady(event) {
+      event.target.setVolume(30);
+      event.target.playVideo();
+      event.target.setPlaybackQuality('highres');
+    }
+
+    function onPlayerStateChange(event) {
+      if (event.data == YT.PlayerState.BUFFERING) {
+        event.target.setPlaybackQuality('highres');
+      }
+    }
+  })
+
+  let ytbgArr = [ytBack, ytCross];
+  ytbgArr.forEach(item => {
+    item.addEventListener('click', (e) => {
+      backgro.classList.remove('active');
+      ytBack.classList.remove('active');
+      body.style.overflow = 'scroll';
+      player.destroy();
+    });
+  });
 })
 
 
 //- swiper
 
 const swiper = new Swiper('.swiper', {
-    // Настройки Swiper
-    loop: true, // Бесконечная прокрутка
-    pagination: {
-        el: '.slider__pagination',
-        clickable: true, // Делает пагинацию кликабельной
-    },
-    navigation: {
-        nextEl: '.slider__btn-next',
-        prevEl: '.slider__btn-prev',
-    },
+  // Настройки Swiper
+  loop: true, // Бесконечная прокрутка
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true, // Делает пагинацию кликабельной
+  },
+  navigation: {
+    nextEl: '.slider__btn-next',
+    prevEl: '.slider__btn-prev',
+  },
 });
 
 
@@ -350,43 +396,51 @@ const swiper = new Swiper('.swiper', {
 
 
 function TestDriveFn() {
-    //e.preventDefault();
-    document.getElementById('testdrive').scrollIntoView({
-        block: "start",
-        behavior: 'smooth'
-    })
+  //e.preventDefault();
+  document.getElementById('testdrive').scrollIntoView({
+    block: "start",
+    behavior: 'smooth'
+  })
 }
 
 function decisionsFn() {
-    //e.preventDefault();
-    document.getElementById('decisions').scrollIntoView({
-        block: "start",
-        behavior: 'smooth'
-    })
+  //e.preventDefault();
+  document.getElementById('decisions').scrollIntoView({
+    block: "start",
+    behavior: 'smooth'
+  })
 }
 
 function learningFn() {
-    //e.preventDefault();
-    document.getElementById('learning').scrollIntoView({
-        block: "start",
-        behavior: 'smooth'
-    })
+  //e.preventDefault();
+  document.getElementById('learning').scrollIntoView({
+    block: "start",
+    behavior: 'smooth'
+  })
 }
 
 function trustFn() {
-    //e.preventDefault();
-    document.getElementById('trust').scrollIntoView({
-        block: "start",
-        behavior: 'smooth'
-    })
+  //e.preventDefault();
+  document.getElementById('trust').scrollIntoView({
+    block: "start",
+    behavior: 'smooth'
+  })
 }
 
 function questionsFn() {
-    //e.preventDefault();
-    document.getElementById('questions').scrollIntoView({
-        block: "start",
-        behavior: 'smooth'
-    })
+  //e.preventDefault();
+  document.getElementById('questions').scrollIntoView({
+    block: "start",
+    behavior: 'smooth'
+  })
+}
+
+function moreFn() {
+  //e.preventDefault();
+  document.getElementById('form-down').scrollIntoView({
+    block: "start",
+    behavior: 'smooth'
+  })
 }
 
 
