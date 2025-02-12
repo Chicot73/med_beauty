c837b4b7b925df6ac14393a7064e23a6617e62e4
igor <admin@igorbobyrev.ru>
2025-02-12 18:14:46 +0300

    12.02

---
+++ public/finish.php
@@ -0,0 +1,75 @@
+<?php
+// Файлы phpmailer
+
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+$json = file_get_contents('php://input');
+$obj = json_decode($json, true);
+$name = $obj['name'];
+$nameF = $obj['name_f'];
+$phone = $obj['phone'];
+$phoneF = $obj['phone_f'];
+$question = $obj['question'];
+$questionF = $obj['question_f'];
+$agree = $obj['agree'];
+
+// Формирование самого письма
+$title = "Новый контакт".$name.$nameF;
+
+$body = "<h2>Новый контакт на Landing page</h2>
+
+  <tr style='background-color: #f8f8f8;'>
+    <td style='padding: 10px; border: #e9e9e9 1px solid;'><b>Клиент: </b>$name $nameF</td>
+  </tr>
+  <tr style='background-color: #ffffff;'>
+    <td style='padding: 10px; border: #e9e9e9 1px solid;'><b>Телефон: </b>$phone $phoneF</td>
+  </tr>
+  <tr style='background-color: #f8f8f8;'>
+    <td style='padding: 10px; border: #e9e9e9 1px solid;'><b>Вопрос клиента: </b>$question $questionF</td>
+  </tr>
+  <tr style='background-color: #ffffff;'>
+    <td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$agree</b></td>
+  </tr>";
+$body = "<table style='width: 100%;'>$body</table>";
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+
+try {
+  $mail->isSMTP();
+  $mail->CharSet = "UTF-8";
+  $mail->SMTPAuth   = true;
+  //$mail->SMTPDebug = 2; 
+  $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+// Настройки вашей почты
+  $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
+  $mail->Username   = 'igorbob73@gmail.com'; // Логин на почте
+  $mail->Password   = 'lreqgdhqfazzccob'; // Пароль 
+  $mail->SMTPSecure = 'ssl';
+  $mail->Port       = 465;
+
+  $mail->setFrom('igorbob73@gmail.com', 'Игорь Бобырев'); // Адрес самой почты и имя отправителя
+
+  // Получатель письма
+  $mail->addAddress('igorbob73@gmail.com');
+  $mail->addAddress('yorg1973fisher@yandex.ru');
+
+  // Отправка сообщения
+  $mail->isHTML(true);
+  $mail->Subject = $title;
+  $mail->Body = $body;
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {$result = "success";} 
+else {$result = "error";}
+
+} catch (Exception $e) {
+  $result = "error";
+  $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+echo json_encode(["result" => $result, "status" => $status]);
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,56 @@
+// Hero-section Анимация появления
+
+const alert = document.querySelector('.hero__alert'),
+  body = document.body || document.getElementsByTagName('body')[0];
+
+
+function alertStart() {
+
+  alert.setAttribute("id", "alert_start");
+  body.style.overflow = 'hidden';
+
+}
+
+function animateHero() {
+
+  let m1 = document.querySelector('.hero__module01'),
+    m2 = document.querySelector('.hero__module02'),
+    m3 = document.querySelector('.hero__module03'),
+    m4 = document.querySelector('.hero__module04'),
+    bigObg = document.querySelector('.hero__bigobj'),
+    fog = document.querySelector('.hero__fog'),
+    blur = document.querySelector('.hero__blur');
+  m1.setAttribute("id", "module01");
+  m2.setAttribute("id", "module02");
+  m3.setAttribute("id", "module03");
+  m4.setAttribute("id", "module04");
+  bigObg.setAttribute("id", "bigdevice");
+  fog.setAttribute("id", "fog");
+  blur.setAttribute("id", "blur");
+  //setTimeout(alertStart, 2000);
+}
+
+// закрытие окна про куки
+
+document.addEventListener('click', alertFinish);
+
+function alertFinish(ev) {
+
+  const area = ev.target.closest("#sub");
+  const area2 = ev.target.closest("#unsub");
+
+  if ((area) || (area2)) {
+
+    alert.removeAttribute("id", "alert_start");
+    animateHero();
+    document.removeEventListener('scroll', alertStart);
+    body.style.overflow = 'scroll';
+  }
+}
+
+document.addEventListener('scroll', alertStart);
+
+
 //выбор языка
 
 let langwRandome = document.querySelector('.header__chose-lang'),
@@ -46,214 +99,7 @@ function listener(event) {
 
 
 
-// бургер-меню
 
-let burgerMenu = document.querySelector('.header__burger'),
-  burgerBtn = document.querySelector('.header__burger-btn'),
-  burgerClose = document.querySelector('.header__cross'),
-  headerCover = document.querySelector('.header__cover'), //затемнение на фоне
-  burgerLinks = document.getElementById('header_links'),
-  upForm = document.querySelector(".header__form"),
-  burgerGetinfo = document.querySelector('.header__form-btn'),
-  body = document.body || document.getElementsByTagName('body')[0];
-
-burgerBtn.addEventListener('click', (e) => {
-
-  if (burgerMenu.classList.contains('header__burger--out')) {
-    burgerMenu.classList.remove('header__burger--out');
-  };
-  burgerMenu.classList.add('header__burger--in');
-  headerCover.style.display = 'block';
-  body.style.overflow = 'hidden';
-
-  // document.addEventListener('click', nonFormClick);
-
-  e.stopImmediatePropagation()
-});
-
-//Логика после открытия формы, если хотят закрыть его, нажав на пустое пространство вне формы
-
-
-// function nonFormClick(e) {
-
-//   let upformField = e.target.closest.upForm;
-
-//   if (!upformField) {
-//     burgerMenu.classList.remove('header__burger--in');
-//     upForm.style.display = 'none';
-//     headerCover.style.display = 'none';
-//     body.style.overflow = 'scroll';
-//   }
-// }
-
-//Логика после открытия бургер-меню, если хотят закрыть его
-
-let puArr = [burgerClose, burgerLinks].forEach(item => {
-  item.addEventListener('click', (e) => {
-    burgerMenu.classList.remove('header__burger--in');
-    headerCover.style.display = 'none';
-    body.style.overflow = 'scroll';
-    e.stopImmediatePropagation();
-  });
-});
-
-burgerGetinfo.addEventListener('click', (e) => {
-  burgerMenu.classList.remove('header__burger--in');
-  upForm.style.display = 'block';
-  // e.stopImmediatePropagation();
-});
-
-
-
-
-// верхняя форма
-
-let FormBtn = document.querySelectorAll('.form__btn');
-
-
-
-document.addEventListener('click', (e) => {
-
-  const target = e.target.closest('.form__btn');
-
-  if (target) {
-
-    burgerMenu.classList.remove('header__burger--in');
-    upForm.style.display = 'block';
-    // e.stopImmediatePropagation();
-
-    sendForm();
-  }
-})
-
-
-
-//Маска формы
-
-let telSelector = document.getElementById('phone'),
-  nameSelector = document.getElementById('name'),
-  questionSelector = document.getElementById('question'),
-  telSelectorDown = document.getElementById('phone-f'),
-  nameSelectorDown = document.getElementById('name-f'),
-  questionSelectorDown = document.getElementById('question-f');
-
-// Inputmask({ "mask": "+9[99](9[9999])[9[9999]]-[9[9999]]", "greedy": "false" }).mask(telSelector);
-// Inputmask({ "mask": "+9{1,20}/(/)/.-", "greedy": "false" }).mask(telSelectorDown);
-
-const telValid = new Inputmask({
-  mask: "+*{1,20}",
-  greedy: false,
-  definitions: {
-    '*': {
-      validator: "[0-9\u0028\u0029\u002d]",//валидация пропускает только числа от 0 до 9, дефис и скобки
-    }
-  }
-});
-
-telValid.mask(telSelector); //применяем сформированную маску для указанного инпута emailSelector
-
-let sendForm = function () {
-
-  document.addEventListener('submit', function (event) {
-    event.preventDefault();
-    let long = telSelector.value,
-      long2 = nameSelector.value,
-      long3 = questionSelector.value;
-    long4 = telSelectorDown.value;
-    long5 = nameSelectorDown.value;
-    long6 = questionSelectorDown.value;
-    let longNum = long.replace(/[^0-9]/g, ""); //удаляем из строки всё, кроме цифр  // ошибка из-за этого
-    let longNum2 = long4.replace(/[^0-9]/g, "");
-
-    if ((document.getElementById('agree').checked) || (document.getElementById('agree-f').checked)) {
-
-      if ((((longNum.length > 2) && (longNum.length < 50)) && ((long2.length > 1) && (long2.length < 36)) && ((long3.length > 2) && (long3.length < 1501))) || (((longNum2.length > 2) && (longNum2.length < 50)) && ((long5.length > 1) && (long5.length < 36)) && ((long6.length > 2) && (long6.length < 1501)))) {
-
-        document.getElementById('warning').style.display = "block";
-        document.getElementById('warning-f').style.display = "block";
-        document.getElementById('warning').style.color = "#49ff0c";
-        document.getElementById('warning-f').style.color = "#49ff0c";
-        document.getElementById('warning').textContent = "Спасибо, мы свяжемся с Вами!";
-        document.getElementById('warning-f').textContent = "Спасибо, мы свяжемся с Вами!";
-
-        function delText() {
-          document.getElementById('warning').style.display = "none";
-          document.getElementById('warning-f').style.display = "none";
-        }
-        setTimeout(delText, 2000);
-
-        function goodby() {
-
-          document.querySelector('.header__form').style.display = "none";
-          headerCover.style.display = 'none';
-          body.style.overflow = 'scroll';
-
-        }
-        setTimeout(goodby, 2000);
-
-      } else {
-
-        document.getElementById('warning').textContent = "Пожалуйста введите правильные данные";
-        document.getElementById('warning-f').textContent = "Пожалуйста введите правильные данные";
-        document.getElementById('warning').style.display = "block";
-        document.getElementById('warning-f').style.display = "block";
-
-      }
-    } else {
-
-      document.getElementById('warning').textContent = "Пожалуйста дайте свое согласие на обработку персональных данных";
-      document.getElementById('warning-f').textContent = "Пожалуйста дайте свое согласие на обработку персональных данных";
-      document.getElementById('warning').style.display = "block";
-      document.getElementById('warning-f').style.display = "block";
-    }
-  })
-}
-
-
-// Hero-section Анимация появления
-
-alert = document.querySelector('.hero__alert');
-
-function alertStart() {
-
-  alert.setAttribute("id", "alert_start");
-
-}
-
-function loadPage() {
-
-  let m1 = document.querySelector('.hero__module01'),
-    m2 = document.querySelector('.hero__module02'),
-    m3 = document.querySelector('.hero__module03'),
-    m4 = document.querySelector('.hero__module04'),
-    bigObg = document.querySelector('.hero__bigobj'),
-    fog = document.querySelector('.hero__fog'),
-    blur = document.querySelector('.hero__blur');
-  m1.setAttribute("id", "module01");
-  m2.setAttribute("id", "module02");
-  m3.setAttribute("id", "module03");
-  m4.setAttribute("id", "module04");
-  bigObg.setAttribute("id", "bigdevice");
-  fog.setAttribute("id", "fog");
-  blur.setAttribute("id", "blur");
-  // setTimeout(alertStart, 2000);
-}
-
-// закрытие окна про куки
-
-function alertFinish(ev) {
-
-  const area = ev.target.closest("#sub");
-  const area2 = ev.target.closest("#unsub");
-
-  if ((area) || (area2)) {
-
-    alert.removeAttribute("id", "alert_start");
-    loadPage();
-  }
-}
-
-body.addEventListener('click', alertFinish);
 
 
 //accordeon
@@ -442,6 +288,269 @@ function moreFn() {
     behavior: 'smooth'
   })
 }
+
+
+
+
+// бургер-меню
+
+let burgerMenu = document.querySelector('.header__burger'),
+  burgerBtn = document.querySelector('.header__burger-btn'),
+  burgerCloseBtn = document.querySelector('.header__cross'),
+  headerCover = document.querySelector('.header__cover'), //затемнение на фоне
+  burgerLinks = document.getElementById('header_links'),
+  upForm = document.querySelector(".header__form"),
+  burgerGetinfo = document.querySelector('.header__form-btn');
+
+burgerBtn.addEventListener('click', (e) => {
+
+  if (burgerMenu.classList.contains('header__burger--out')) {
+    burgerMenu.classList.remove('header__burger--out');
+  };
+  burgerMenu.classList.add('header__burger--in');
+  headerCover.style.display = 'block';
+  body.style.overflow = 'hidden';
+
+  // document.addEventListener('click', nonFormClick);
+
+  e.stopImmediatePropagation()
+});
+
+
+
+// Логика после открытия бургер - меню, если хотят закрыть его
+
+let closeBurger = function () {
+
+  burgerMenu.classList.remove('header__burger--in');
+  headerCover.style.display = 'none';
+  body.style.overflow = 'scroll';
+  // e.stopImmediatePropagation();
+
+}
+
+document.addEventListener('click', (e) => {
+
+  let mBurger = e.composedPath().includes(burgerMenu);
+  let formBtnClicked = e.composedPath().includes(burgerGetinfo);
+  let noField = false;
+  let FieldForm = e.composedPath().includes(upForm);
+  let noFieldForm = false;
+
+  // const withinBoundaries = e.composedPath().includes(div);
+
+  if (formBtnClicked) {
+
+    burgerMenu.classList.remove('header__burger--in');
+    upForm.style.display = 'block';
+
+  }
+
+  if (!mBurger) {
+    noField = true;
+  }
+
+  let puArr = [
+
+    e.composedPath().includes(burgerCloseBtn),
+    e.composedPath().includes(burgerLinks)
+
+  ];
+
+  puArr.forEach(item => {
+    if (item) { closeBurger(); }
+  })
+
+
+  if (noField === true) {
+
+    if (!FieldForm) {
+      noFieldForm = true;
+      upForm.style.display = 'none';
+      closeBurger();
+    }
+
+    // document.addEventListener('click', (e) => {
+
+
+    // })
+
+    // if (noFieldForm) {
+
+    //   // Логика после открытия формы, если хотят закрыть его, нажав на пустое пространство вне формы
+
+    //   closeBurger();
+    // }
+  }
+})
+
+
+
+
+
+
+
+// let puArr = [burgerCloseBtn, burgerLinks].forEach(item => {
+//   item.addEventListener('click', (e) => {
+//     burgerMenu.classList.remove('header__burger--in');
+//     headerCover.style.display = 'none';
+//     body.style.overflow = 'scroll';
+//     e.stopImmediatePropagation();
+//   });
+// });
+
+
+
+
+
+///////////////////
+
+// верхняя форма
+
+let FormBtn = document.querySelectorAll('.form__btn');
+
+let f_formSucess = false;
+
+
+//Маска формы
+
+let telSelector = document.getElementById('phone'),
+  nameSelector = document.getElementById('name'),
+  questionSelector = document.getElementById('question'),
+  telSelectorDown = document.getElementById('phone-f'),
+  nameSelectorDown = document.getElementById('name-f'),
+  questionSelectorDown = document.getElementById('question-f');
+
+// Inputmask({ "mask": "+9[99](9[9999])[9[9999]]-[9[9999]]", "greedy": "false" }).mask(telSelector);
+// Inputmask({ "mask": "+9{1,20}/(/)/.-", "greedy": "false" }).mask(telSelectorDown);
+
+const telValid = new Inputmask({
+  mask: "+*{1,20}",
+  greedy: false,
+  definitions: {
+    '*': {
+      validator: "[0-9\u0028\u0029\u002d]",//валидация пропускает только числа от 0 до 9, дефис и скобки
+    }
+  }
+});
+
+telValid.mask(telSelector); //применяем сформированную маску для указанного инпута emailSelector
+
+
+
+let validForm = function () {
+
+  let long = telSelector.value,
+    long2 = nameSelector.value,
+    long3 = questionSelector.value;
+  long4 = telSelectorDown.value;
+  long5 = nameSelectorDown.value;
+  long6 = questionSelectorDown.value;
+  let longNum = long.replace(/[^0-9]/g, ""); //удаляем из строки всё, кроме цифр  // ошибка из-за этого
+  let longNum2 = long4.replace(/[^0-9]/g, "");
+
+  if ((document.getElementById('agree').checked) || (document.getElementById('agree-f').checked)) {
+
+    if ((((longNum.length > 2) && (longNum.length < 50)) && ((long2.length > 1) && (long2.length < 36)) && ((long3.length > 2) && (long3.length < 1501))) || (((longNum2.length > 2) && (longNum2.length < 50)) && ((long5.length > 1) && (long5.length < 36)) && ((long6.length > 2) && (long6.length < 1501)))) {
+
+      document.getElementById('warning').style.display = "block";
+      document.getElementById('warning-f').style.display = "block";
+      document.getElementById('warning').style.color = "#49ff0c";
+      document.getElementById('warning-f').style.color = "#49ff0c";
+      document.getElementById('warning').textContent = "Спасибо, мы свяжемся с Вами!";
+      document.getElementById('warning-f').textContent = "Спасибо, мы свяжемся с Вами!";
+
+      function delText() {
+        document.getElementById('warning').style.display = "none";
+        document.getElementById('warning-f').style.display = "none";
+      }
+      setTimeout(delText, 2000);
+
+      function goodby() {
+
+        document.querySelector('.header__form').style.display = "none";
+        headerCover.style.display = 'none';
+        body.style.overflow = 'scroll';
+
+      }
+      setTimeout(goodby, 2000);
+
+      console.log('валидация прошла успешно');
+
+      f_formSucess = true;
+
+    } else {
+
+      console.log('форма не прошла по валидации');
+
+
+      document.getElementById('warning').textContent = "Пожалуйста введите правильные данные";
+      document.getElementById('warning-f').textContent = "Пожалуйста введите правильные данные";
+      document.getElementById('warning').style.display = "block";
+      document.getElementById('warning-f').style.display = "block";
+
+    }
+  } else {
+
+    console.log('форма не прошла по валидации');
+
+    document.getElementById('warning').textContent = "Пожалуйста дайте свое согласие на обработку персональных данных";
+    document.getElementById('warning-f').textContent = "Пожалуйста дайте свое согласие на обработку персональных данных";
+    document.getElementById('warning').style.display = "block";
+    document.getElementById('warning-f').style.display = "block";
+  }
+}
+
+document.addEventListener('click', (e) => {
+
+  const target = e.target.closest('.form__btn');
+
+  if (target) {
+
+    closeBurger();
+
+    validForm();
+  }
+})
+
+const form = document.getElementById('#form');
+
+const sendFormTo = (data) => {
+  return fetch('../finish.php', {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8'
+    }
+  }).then(res => res.json());
+}
+
+let submitForm = function (event) {
+
+  event.preventDefault();
+
+  console.log('submit');
+
+
+  if (f_formSucess === true) {
+
+    const dataForm = new FormData(form);
+    const user = {};
+
+    dataForm.forEach((val, key) => {
+      user[key] = val;
+    });
+
+    sendFormTo(user).then(data => {
+      console.log('Письмо отправлено успешно');
+
+      form.reset();
+
+      f_formSucess = false;
+    })
+  }
+}
+
 
 
 
